14dbf44df3fc2b18e82fd86e1c60bca77068b379
yesheng <dasheng523@163.com>
2019-04-20 18:14:28 +0800

    暂时屏蔽neo-conn

--- src/main/db.clj
+++ src/main/db.clj
@@ -4,7 +4,7 @@
             [neo4j-clj.core :as db :refer [defquery]]
             [main.config :refer [env]]))
 
-(defstate neo-conn
+#_(defstate neo-conn
     :start
     (do (log/info "连接neo4j数据库...")
         (db/connect (:neo4j-url env) (:neo4j-user env) (:neo4j-pass env))))
